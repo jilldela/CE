c163fa0ce43e086a010792e13ea72605c1165e08
jilldela <jilldelosangeles@gmail.com>
2017-07-28 16:53:15 -0700

    Add brute force solution to two_sum

--- problems/array.rb
+++ problems/array.rb
@@ -7,5 +7,34 @@ def sum_rec(arr)
   arr[0] + sum_rec(arr[1..-1])
 end
 
-p sum_rec( [1, 2, 3] ) # == 6
-p sum_rec( [-1, 5, 7, -3] ) # == 8
+# p sum_rec( [1, 2, 3] ) # == 6
+# p sum_rec( [-1, 5, 7, -3] ) # == 8
+
+# ------------------------------
+
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Given nums = [2, 7, 11, 15], target = 9,
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+def two_sum(nums, target)
+  nums.each_with_index do |num, idx|
+    remainder = target - num
+    
+    i = 0
+    next if i == idx
+    
+    while i < nums.length
+      if nums[i] == remainder
+          return [idx, i]
+      end
+      i += 1
+    end
+  end
+end
+
+p two_sum([1, 3, 5, 7], 10) # == 1, 3
+
+# bad implementation because inefficient
+# O(n ^ 2)
