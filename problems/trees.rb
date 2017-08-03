10497661003a4c889ee26d79c820f42069f62788
jilldela <jilldelosangeles@gmail.com>
2017-08-03 12:42:20 -0700

    Add max_depth solution

--- problems/trees.rb
+++ problems/trees.rb
@@ -267,6 +267,16 @@ def is_symmetric(root)
     true
 end
 
+ # --------------------------
+# Given a binary tree, find its maximum depth.
+
+# The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+def max_depth(root)
+    return 0 if root.nil?
+    
+    1 + [max_depth(root.left), max_depth(root.right)].max
+end
 
 
 
