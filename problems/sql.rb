dcf96d975b8823f98adf7bbb5ad7dcdd215aa8fa
jilldela <jilldelosangeles@gmail.com>
2017-08-27 17:59:33 -0700

    Add duplicate email query

--- problems/sql.rb
+++ problems/sql.rb
@@ -50,3 +50,30 @@ SET sex =
         WHEN 'm' THEN 'f'
         ELSE 'm'
     END
+
+# ----------------------------------
+# Write a SQL query to find all duplicate emails in a table named Person.
+
+# +----+---------+
+# | Id | Email   |
+# +----+---------+
+# | 1  | a@b.com |
+# | 2  | c@d.com |
+# | 3  | a@b.com |
+# +----+---------+
+# For example, your query should return the following for the above table:
+
+# +---------+
+# | Email   |
+# +---------+
+# | a@b.com |
+# +---------+
+
+SELECT
+    Email
+FROM
+    Person
+GROUP BY
+    Email
+HAVING
+    COUNT(Email) > 1
