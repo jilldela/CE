b124cc59d05c288e9123dba90060ccedd493d161
jilldela <jilldelosangeles@gmail.com>
2017-09-19 16:36:46 -0700

    add count_islands prompt

--- problems/dynamic_programming.rb
+++ problems/dynamic_programming.rb
@@ -111,3 +111,13 @@ def min_path_sum(grid)
 
     grid[-1][-1]
 end
+
+# Given a boolean 2D matrix, find the number of islands. A group of connected trues forms an island. For example, the below matrix contains 5 islands.
+
+# Input : mat =     [[T, T, F, F, F],
+#                    [F, T, F, F, T],
+#                    [T, F, F, T, T],
+#                    [F, F, F, F, F],
+#                    [T, F, T, F, T]]
+# Output : 5
+
